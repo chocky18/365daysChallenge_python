835b36441b2aa8e37ed20609cc1085afa32a0b8b
chocky18 <chockynaresh18@gmail.com>
2024-08-09 11:53:56 +0530

    select every alternate element

---
+++ select_every_alternate_element.py
@@ -0,0 +1,9 @@
+'''
+Given a list, write a single line of code to select every alternate element,
+either starting from the first element or the second element.
+For example, if the list is a = [1, 2, 3, 4, 5, 6], how can you select [1, 3, 5] or [2, 4, 6]
+'''
+
+a = [1, 2, 3, 4, 5, 6]
+print(a[::2])
+print(a[1::2])
